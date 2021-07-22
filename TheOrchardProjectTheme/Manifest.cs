ccdeb23a2e708171bbc7a0572996012dc57c1fec
Antoine Griffard <agriffard@hotmail.com>
2021-07-22 21:06:49 +0200

    Update version to 1.0.0

--- TheOrchardProjectTheme/Manifest.cs
+++
@@ -1,9 +0,0 @@
-using OrchardCore.DisplayManagement.Manifest;
-
-[assembly: Theme(
-    Name = "The Orchard Project Theme",
-    Author = "The Orchard Team",
-    Website = "https://orchardproject.net",
-    Version = "2.0.0",
-    Description = "A theme for Orchard Project site."
-)]
