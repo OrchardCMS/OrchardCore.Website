2c91269aee3cc6f66f4eb44b340ac84f63b3962a
Antoine Griffard <agriffard@hotmail.com>
2024-04-25 19:13:16 +0200

    Website theme, timeline image position

---
+++ Themes/OCWS/Manifest.cs
@@ -0,0 +1,10 @@
+using OrchardCore.DisplayManagement.Manifest;
+
+[assembly: Theme(
+    Name = "Orchard Core Website",
+    BaseTheme = "TheTheme",
+    Author = "The Orchard Team",
+    Website = "https://orchardproject.net",
+    Version = "1.0.0",
+    Description = "A theme for the Orchard Core site."
+)]
