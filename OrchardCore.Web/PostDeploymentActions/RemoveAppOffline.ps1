eeb3832a4c2bc7684bfac5373e86adfcbf1fd541
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2025-02-24 02:12:16 +0100

    Can't deploy PostDeploymentActions, trying it by hand

--- OrchardCore.Web/PostDeploymentActions/RemoveAppOffline.ps1
+++
@@ -1,4 +0,0 @@
-﻿Remove-Item -Path '../../../wwwroot/app_offline.htm'
-
-# Removing this script so the next deployment of the app_offline.htm won't get deleted.
-Remove-Item -Path $MyInvocation.MyCommand.Path -Force
---
+++ PostDeploymentActions/RemoveAppOffline.ps1
@@ -0,0 +1,14 @@
+﻿#$wwwrootPath = '../../../wwwroot'
+$wwwrootPath = 'wwwroot'
+$appOfflinePath = $wwwrootPath + '/app_offline.htm'
+$markerFilePath = $wwwrootPath + '/app_offline was last deployment.txt'
+
+if (Test-Path -Path $markerFilePath)
+{
+    Remove-Item -Path $markerFilePath
+    Remove-Item -Path $appOfflinePath
+}
+elseif (Test-Path -Path $appOfflinePath)
+{
+    New-Item -Path $markerFilePath -ItemType File -Force
+}
